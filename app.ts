b95124d65e8c2d6c96e2759b62139218badc6943
Alexander <alex.esbj@gmail.com>
2023-01-16 11:31:09 +0100

    Object commmit

--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-const djur:string[] = ["apa", "orangutang","gorilla", "get","giraff"]
+const djur: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
 console.log(djur);
+
+const o: { name: string; age: number; isAwesome: boolean } = {
+  name: "Gugge",
+  age: 57,
+  isAwesome: true
+};
